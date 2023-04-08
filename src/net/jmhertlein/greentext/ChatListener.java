84a1da3c9205713a290ce858a7de45a3bd00d227
rand0m0User <79854039+rand0m0User@users.noreply.github.com>
2023-04-08 10:49:14 -0700

    add NetBeans project

--- src/net/jmhertlein/greentext/ChatListener.java
+++ src/net/jmhertlein/greentext/ChatListener.java
@@ -6,28 +6,20 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatListener implements Listener {
-   @EventHandler
-   public void onPlayerChat(AsyncPlayerChatEvent e) {
-	  String message = e.getMessage();
-      if (message.startsWith(">")) {
-         e.setMessage(ChatColor.GREEN + message);
-		 return;
-      }
-	  if (message.startsWith("<")) {
-         e.setMessage(ChatColor.GOLD + message);
-		 return;
-      }
-	  if (message.startsWith("==") && message.endsWith("==")) {
-         e.setMessage(ChatColor.RED + message.replace("==", ""));
-		 return;
-      }
-	  if (message.startsWith("--") && message.endsWith("--")) {
-         e.setMessage(ChatColor.BLUE + message.replace("--", ""));
-		 return;
-      }
-	  if (message.startsWith("%%") && message.endsWith("%%")) {
-         e.setMessage(ChatColor.BOLD + "" + ChatColor.GREEN + message.replace("%%", ""));
-		 return;
-      }
-   }
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent e) {
+        String message = e.getMessage();
+        if (message.startsWith(">")) {
+            e.setMessage(ChatColor.GREEN + message);
+        } else if (message.startsWith("<")) {
+            e.setMessage(ChatColor.GOLD + message);
+        } else if (message.startsWith("==") && message.endsWith("==")) {
+            e.setMessage(ChatColor.RED + message.replace("==", ""));
+        } else if (message.startsWith("--") && message.endsWith("--")) {
+            e.setMessage(ChatColor.BLUE + message.replace("--", ""));
+        } else if (message.startsWith("%%") && message.endsWith("%%")) {
+            e.setMessage(ChatColor.BOLD + "" + ChatColor.GREEN + message.replace("%%", ""));
+        }
+    }
 }
--- src/net/jmhertlein/greentext/GreentextPlugin.java
+++ src/net/jmhertlein/greentext/GreentextPlugin.java
@@ -3,7 +3,8 @@ package net.jmhertlein.greentext;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GreentextPlugin extends JavaPlugin {
-   public void onEnable() {
-      this.getServer().getPluginManager().registerEvents(new ChatListener(), this);
-   }
+
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(new ChatListener(), this);
+    }
 }
