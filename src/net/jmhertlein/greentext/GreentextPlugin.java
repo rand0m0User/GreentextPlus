c92a0e779d3a3e1b90efd2b7d5b4389b57d3fde3
rand0m0User <79854039+rand0m0User@users.noreply.github.com>
2023-04-07 19:38:25 -0700

    remove unused import

--- src/net/jmhertlein/greentext/GreentextPlugin.java
+++ src/net/jmhertlein/greentext/GreentextPlugin.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.mcstats.Metrics;
 
 public class GreentextPlugin extends JavaPlugin {
    public void onEnable() {
