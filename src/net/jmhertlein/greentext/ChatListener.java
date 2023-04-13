44209f43bf598b9abf54c14a21365d5e0b5bcf2e
rand0m0User <79854039+rand0m0User@users.noreply.github.com>
2023-04-13 08:49:26 -0700

    add strikethrough, minor code cleanup

--- src/net/jmhertlein/greentext/ChatListener.java
+++ src/net/jmhertlein/greentext/ChatListener.java
@@ -7,25 +7,34 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatListener implements Listener {
 
+    String RED = "==";
+    String CALM = "--";
+    String DATAMINING = "%%";
+    String CROSSOUT = "~~";
+
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent e) {
         String message = e.getMessage();
         if (message.startsWith(">")) {
             e.setMessage(ChatColor.GREEN + message);
         } else if (message.startsWith("<")) {
-            e.setMessage(ChatColor.GOLD + message);
-        } else if (message.startsWith("==")
-                && message.endsWith("==")
-                && message.replace("==", "").length() != 0) {
-            e.setMessage(ChatColor.RED + message.replace("==", ""));
-        } else if (message.startsWith("--")
-                && message.endsWith("--")
-                && message.replace("--", "").length() != 0) {
-            e.setMessage(ChatColor.BLUE + message.replace("--", ""));
-        } else if (message.startsWith("%%")
-                && message.endsWith("%%")
-                && message.replace("%%", "").length() != 0) {
-            e.setMessage(ChatColor.DARK_GREEN + message.replace("%%", ""));
+            e.setMessage(ChatColor.GOLD + message); //KUUUUUUUZ ADD ChatColor.ORANGE
+        } else if (message.startsWith(RED)
+                && message.endsWith(RED)
+                && message.replace(RED, "").length() != 0) { //fixed bug where "====" would result in an empty message
+            e.setMessage(ChatColor.RED + message.replace(RED, ""));
+        } else if (message.startsWith(CALM)
+                && message.endsWith(CALM)
+                && message.replace(CALM, "").length() != 0) {
+            e.setMessage(ChatColor.BLUE + message.replace(CALM, ""));
+        } else if (message.startsWith(DATAMINING)
+                && message.endsWith(DATAMINING)
+                && message.replace(DATAMINING, "").length() != 0) {
+            e.setMessage(ChatColor.DARK_GREEN + message.replace(DATAMINING, ""));
+        } else if (message.startsWith(CROSSOUT)
+                && message.endsWith(CROSSOUT)
+                && message.replace(CROSSOUT, "").length() != 0) {
+            e.setMessage(ChatColor.STRIKETHROUGH + message.replace(CROSSOUT, ""));
         }
     }
 }
