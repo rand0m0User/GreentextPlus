4fbe5f3c0427c0f43f39938bf2ffc1224d7cd804
rand0m0User <79854039+rand0m0User@users.noreply.github.com>
2023-05-24 15:01:38 -0700

    Update ChatListener.java
    fixed streetshitter teir bug

--- src/net/jmhertlein/greentext/ChatListener.java
+++ src/net/jmhertlein/greentext/ChatListener.java
@@ -24,61 +24,86 @@ public class ChatListener implements Listener {
     public void onPlayerChat(AsyncPlayerChatEvent e) {
         String msg = e.getMessage();
         //msg.replaceAll(":skull:", "\u2620"); //i will NOT learn regex!
-
-        if (msg.startsWith(">")) {
-            e.setMessage(ChatColor.GREEN + msg);
-        } else if (msg.startsWith("<")) {
-            e.setMessage(ChatColor.GOLD + msg); //KUUUUUUUZ ADD ChatColor.ORANGE
-        } else if (msg.startsWith("^")) {
-            e.setMessage(ChatColor.DARK_PURPLE + msg);
-        } else if (msg.startsWith(PINK)
-                && msg.endsWith(PINK)
-                && msg.replace(PINK, "").length() != 0) {
-            e.setMessage(ChatColor.LIGHT_PURPLE + msg.replace(PINK, ""));
-        } else if (msg.startsWith(RED)
-                && msg.endsWith(RED)
-                && msg.replace(RED, "").length() != 0) { //fixed bug where "====" would result in an empty message
-            e.setMessage(ChatColor.RED + msg.replace(RED, ""));
-        } else if (msg.startsWith(RED2)
-                && msg.endsWith(RED2)
-                && msg.replace(RED2, "").length() != 0) {
-            e.setMessage(ChatColor.DARK_RED + msg.replace(RED2, ""));
-        } else if (msg.startsWith(CALM1)
-                && msg.endsWith(CALM1)
-                && msg.replace(CALM1, "").length() != 0) {
-            e.setMessage(ChatColor.AQUA + msg.replace(CALM1, ""));
-        } else if (msg.startsWith(CALM)
-                && msg.endsWith(CALM)
-                && msg.replace(CALM, "").length() != 0) {
-            e.setMessage(ChatColor.BLUE + msg.replace(CALM, ""));
-        } else if (msg.startsWith(CALM3)
-                && msg.endsWith(CALM3)
-                && msg.replace(CALM3, "").length() != 0) {
-            e.setMessage(ChatColor.DARK_BLUE + msg.replace(CALM3, ""));
-        } else if (msg.startsWith(YELLOW)
-                && msg.endsWith(YELLOW)
-                && msg.replace(YELLOW, "").length() != 0) {
-            e.setMessage(ChatColor.YELLOW + msg.replace(YELLOW, ""));
-        } else if (msg.startsWith(DATAMINING)
-                && msg.endsWith(DATAMINING)
-                && msg.replace(DATAMINING, "").length() != 0) {
-            e.setMessage(ChatColor.DARK_GREEN + msg.replace(DATAMINING, ""));
-        } else if (msg.startsWith(CROSSOUT)
-                && msg.endsWith(CROSSOUT)
-                && msg.replace(CROSSOUT, "").length() != 0) {
-            e.setMessage(ChatColor.STRIKETHROUGH + msg.replace(CROSSOUT, ""));
-        } else if (msg.startsWith(UNDERLINE)
-                && msg.endsWith(UNDERLINE)
-                && msg.replace(UNDERLINE, "").length() != 0) {
-            e.setMessage(ChatColor.UNDERLINE + msg.replace(UNDERLINE, ""));
-        } else if (msg.startsWith(BOLD)
-                && msg.endsWith(BOLD)
-                && msg.replace(BOLD, "").length() != 0) {
-            e.setMessage(ChatColor.BOLD + msg.replace(BOLD, ""));
-        } else if (msg.startsWith(ITALIC)
-                && msg.endsWith(ITALIC)
-                && msg.replace(ITALIC, "").length() != 0) {
-            e.setMessage(ChatColor.ITALIC + msg.replace(ITALIC, ""));
+        
+       switch (msg.charAt(0)) { //attempt at optomizing out if else spam by using a switch jumptable
+            case '>':
+                e.setMessage(ChatColor.GREEN + msg);
+                break;
+            case '<':
+                e.setMessage(ChatColor.GOLD + msg); //KUUUUUUUZ ADD ChatColor.ORANGE
+                break;
+            case '^':
+                e.setMessage(ChatColor.DARK_PURPLE + msg);
+            case '=':
+                if (msg.startsWith(RED2) //be carefull of order of operations next time retard
+                        && msg.endsWith(RED2)
+                        && msg.replace(RED2, "").length() != 0) {
+                    e.setMessage(ChatColor.DARK_RED + msg.replace(RED2, ""));
+                } else if (msg.startsWith(RED)
+                        && msg.endsWith(RED)
+                        && msg.replace(RED, "").length() != 0) { //fixed bug where "====" would result in an empty message
+                    e.setMessage(ChatColor.RED + msg.replace(RED, ""));
+                } else if (msg.startsWith(PINK)
+                        && msg.endsWith(PINK)
+                        && msg.replace(PINK, "").length() != 0) {
+                    e.setMessage(ChatColor.LIGHT_PURPLE + msg.replace(PINK, ""));
+                }
+                break;
+            case '-':
+                if (msg.startsWith(CALM3) //3 '-' 's
+                        && msg.endsWith(CALM3)
+                        && msg.replace(CALM3, "").length() != 0) {
+                    e.setMessage(ChatColor.DARK_BLUE + msg.replace(CALM3, ""));
+                } else if (msg.startsWith(CALM) //2 '-' 's
+                        && msg.endsWith(CALM)
+                        && msg.replace(CALM, "").length() != 0) {
+                    e.setMessage(ChatColor.BLUE + msg.replace(CALM, ""));
+                } else if (msg.startsWith(CALM1) //1 '-' 's
+                        && msg.endsWith(CALM1)
+                        && msg.replace(CALM1, "").length() != 0) {
+                    e.setMessage(ChatColor.AQUA + msg.replace(CALM1, ""));
+                }
+                break;
+            case '|':
+                if (msg.startsWith(YELLOW)
+                        && msg.endsWith(YELLOW)
+                        && msg.replace(YELLOW, "").length() != 0) {
+                    e.setMessage(ChatColor.YELLOW + msg.replace(YELLOW, ""));
+                }
+                break;
+            case '%':
+                if (msg.startsWith(DATAMINING)
+                        && msg.endsWith(DATAMINING)
+                        && msg.replace(DATAMINING, "").length() != 0) {
+                    e.setMessage(ChatColor.DARK_GREEN + msg.replace(DATAMINING, ""));
+                }
+                break;
+            case '~':
+                if (msg.startsWith(CROSSOUT)
+                        && msg.endsWith(CROSSOUT)
+                        && msg.replace(CROSSOUT, "").length() != 0) {
+                    e.setMessage(ChatColor.STRIKETHROUGH + msg.replace(CROSSOUT, ""));
+                }
+                break;
+            case '_':
+                if (msg.startsWith(UNDERLINE)
+                        && msg.endsWith(UNDERLINE)
+                        && msg.replace(UNDERLINE, "").length() != 0) {
+                    e.setMessage(ChatColor.UNDERLINE + msg.replace(UNDERLINE, ""));
+                }
+                break;
+            case '\'':
+                if (msg.startsWith(BOLD)
+                        && msg.endsWith(BOLD)
+                        && msg.replace(BOLD, "").length() != 0) {
+                    e.setMessage(ChatColor.BOLD + msg.replace(BOLD, ""));
+                } else if (msg.startsWith(ITALIC)
+                        && msg.endsWith(ITALIC)
+                        && msg.replace(ITALIC, "").length() != 0) {
+                    e.setMessage(ChatColor.ITALIC + msg.replace(ITALIC, ""));
+                }
+                break;
+            default:
         }
     }
 }
