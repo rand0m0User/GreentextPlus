0cdaa434473fd96bd99f1ac499547de1e527bfa2
rand0m0User <79854039+rand0m0User@users.noreply.github.com>
2023-06-26 15:11:54 -0700

    I ❤ BOILERPLATE
    split crafting addons to a separate repo

--- src/net/jmhertlein/greentext/GreentextPlugin.java
+++ src/net/jmhertlein/greentext/GreentextPlugin.java
@@ -9,7 +9,6 @@ public class GreentextPlugin extends JavaPlugin {
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(new ChatListener(), this); //greentext
         this.getServer().getPluginManager().registerEvents(new BookListener(), this); //anti book ban
-        this.getServer().getPluginManager().registerEvents(new MoCrafting(this), this); //add crafting recipes for gapples and ender stars
     }
     //I ❤ BOILERPLATE
     //I ❤ BOILERPLATE
